22c1f379a0633de6ac3d818b506f4d1dceb16a61
Khasbulat Abdullin <khas.2015@gmail.com>
2023-03-05 09:29:42 +0300

    change corsMiddleware

--- internal/app/middleware/cors_middleware.go
+++ internal/app/middleware/cors_middleware.go
@@ -7,7 +7,7 @@ import (
 func CorsMiddleware(allowedHost string, next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Access-Control-Allow-Credentials", "true")
-		w.Header().Set("Access-Control-Allow-Origin", allowedHost)
+		w.Header().Set("Access-Control-Allow-Origin", r.Header.Get("Origin"))
 		next.ServeHTTP(w, r)
 	},
 	)
