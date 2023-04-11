e0a9ea3eda24c9031a75a9a49d98041f22a2a62d
Staurran <staurran@gmail.com>
2023-04-11 15:17:02 +0300

    fix avatar logic

--- internal/pkg/photo/repository/validation.go
+++ internal/pkg/photo/repository/validation.go
@@ -12,13 +12,18 @@ func (r *PhotoRepository) CheckPhoto(row dataStruct.UserPhoto) error {
 	if err != nil {
 		return err
 	}
-	if count == 5 {
-		err = fmt.Errorf("Too many photos, please delete something")
+	switch count {
+	case 5:
+		err = fmt.Errorf("too many photos, please delete something")
 		return err
+	case 0:
+		row.Avatar = true
+		return nil
 	}
+
 	if row.Avatar {
 		var oldAvatar dataStruct.UserPhoto
-		err = r.db.First(&oldAvatar, "userId=? AND avatar=?", row.UserId, true).Error
+		err = r.db.First(&oldAvatar, "user_id=? AND avatar=?", row.UserId, true).Error
 		if err != nil {
 			return err
 		}
@@ -27,5 +32,29 @@ func (r *PhotoRepository) CheckPhoto(row dataStruct.UserPhoto) error {
 		return nil
 	}
 
+	return nil
+}
+
+func (r *PhotoRepository) CheckDeletedPhoto(row dataStruct.UserPhoto) error {
+	var count int64
+	err := r.db.Table("user_photos").Where("user_id= ?", row.UserId).Count(&count).Error
+	if count == 1 {
+		err = fmt.Errorf("you canr delete all your photos")
+	}
+	if err != nil {
+		return err
+	}
+	if row.Avatar == true {
+		var newAvatar dataStruct.UserPhoto
+		err = r.db.Last(&newAvatar, "user_id = ? AND avatar = ?", row.UserId, false).Error
+		if err != nil {
+			return err
+		}
+		newAvatar.Avatar = true
+		err = r.db.Save(&newAvatar).Error
+		if err != nil {
+			return err
+		}
+	}
 	return nil
 }
