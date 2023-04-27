522b3f4fd9a0ad767fade83e103f8a74f1445d20
Staurran <staurran@gmail.com>
2023-04-27 23:13:19 +0300

    fix json name

--- internal/pkg/filter/structs.go
+++ internal/pkg/filter/structs.go
@@ -3,6 +3,6 @@ package filter
 type FilterInput struct {
 	MinAge    int      `json:"minAge" structs:"minAge"`
 	MaxAge    int      `json:"maxAge" structs:"maxAge"`
-	SearchSex uint     `json:"searchSex" structs:"searchSex"`
+	SearchSex uint     `json:"sexSearch" structs:"sexSearch"`
 	Reason    []string `json:"reason" structs:"reason"`
 }
