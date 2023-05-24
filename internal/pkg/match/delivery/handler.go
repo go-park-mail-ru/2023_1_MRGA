b87a8d03ff55ddc13ed946b7ae68f4bee84aae64
Staurran <staurran@gmail.com>
2023-05-24 19:00:30 +0300

    fix

--- internal/pkg/match/delivery/handler.go
+++ internal/pkg/match/delivery/handler.go
@@ -7,7 +7,6 @@ import (
 	"net/http"
 	"strconv"
 
-
 	"github.com/google/uuid"
 	"github.com/gorilla/mux"
 	"github.com/gorilla/websocket"
@@ -85,7 +84,6 @@ func (h *Handler) AddReaction(w http.ResponseWriter, r *http.Request) {
 	}
 	logger.Log(http.StatusOK, "Success", r.Method, r.URL.Path, false)
 	writer.Respond(w, r, map[string]interface{}{})
-	return
 }
 
 func (h *Handler) DeleteMatch(w http.ResponseWriter, r *http.Request) {
